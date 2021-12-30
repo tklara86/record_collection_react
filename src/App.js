7d589831ea43ae2d713b81c4222881c1d372af7e
Tomasz Klara <tomaszklara@gmail.com>
2021-12-30 22:26:07 +0000

    updated to react router 6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import ReactDOM from "react-dom";
+import {BrowserRouter, Routes, Route, Link, Switch, Router} from "react-router-dom";
+import Layout from "./Layout"
+import Home from "./Home";
+import Contact from "./Contact";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <p>Record Collection</p>
-            </div>
-        );
-    }
+export default function App()  {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="contact" element={<Contact />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App;
+
 
 ReactDOM.render(<App />, document.querySelector("#root"))
